b21999cf06fd2685a080978cb92724c831600983
tfenko <tarasfenko13@gmail.com>
2025-05-19 21:11:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -268,3 +268,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
   updatePrice();
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.querySelector('.hamburger');
+  const navLinks = document.querySelector('.nav-links');
+
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navLinks.classList.toggle('active');
+  });
+
+  // Закрити меню при кліку на пункт
+  document.querySelectorAll('.nav-links li a').forEach(link => {
+    link.addEventListener('click', () => {
+      hamburger.classList.remove('active');
+      navLinks.classList.remove('active');
+    });
+  });
+});
